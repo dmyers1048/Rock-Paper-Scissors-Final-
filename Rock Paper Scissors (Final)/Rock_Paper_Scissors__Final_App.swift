2a1ab06f406b8dcc493c36572fceb01a471ce168
dmyers1048 <dmyers1@bsd220.org>
2024-12-16 00:35:35 -0600

    Initial Commit

---
+++ Rock Paper Scissors (Final)/Rock_Paper_Scissors__Final_App.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_Paper_Scissors__Final_App.swift
+//  Rock Paper Scissors (Final)
+//
+//  Created by Devan Myers on 12/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Rock_Paper_Scissors__Final_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
